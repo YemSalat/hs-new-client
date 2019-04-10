1ebe02a8ac182471ba8d30d22f62cbd23ab99330
Konstantin Levin <yem_salat@hotmail.com>
2019-04-10 23:06:00 +0300

    Feat / add more e2e tests

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -18,6 +18,27 @@ module.exports = {
         console.log('All 4 filters are present')
         browser.assert.equal(result.value.length, 4)
       })
-      .end()
+
+    browser.click('.settings-link')
+    browser.pause(500)
+    browser.assert.visible('.popup')
+    browser.elements('css selector', '.popup-tabs > li', function (result) {
+      console.log('All 4 popup tabs are present')
+      browser.assert.equal(result.value.length, 4)
+    })
+    browser.elements('css selector', '.popup-content:first-child > .label', function (result) {
+      console.log('First popup tab has 6 items on it')
+      browser.assert.equal(result.value.length, 6)
+    })
+    browser.click('.popup-tabs > li[data-tab="favorites"]')
+    browser.pause(500)
+    browser.assert.visible('.popup-content[data-tab="favorites"]')
+    browser.assert.containsText('.popup-content[data-tab="favorites"]', 'У вас нет закладок')
+
+    browser.click('.close-popup')
+    browser.pause(500)
+    browser.assert.hidden('.popup')
+
+    browser.end()
   }
 }
