b5e408377f14624e092cfaa199104e1f20cd813a
Konstantin Levin <yem_salat@hotmail.com>
2019-04-10 22:01:45 +0300

    Tweak / bump build version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 # Set build variables
 export HS_BUILD_DATE=`date`
-export HS_BUILD_NUMBER=127
+export HS_BUILD_NUMBER=128
 export HS_GIT_HASH=`git rev-parse --short HEAD`
 export HS_APP_VERSION=`cat package.json | egrep -o '"version"\: "([^\"]+)' | sed -e 's/"version": "//'`
 export HS_GA_CODE="UA-138087935-1"
